156a2ef013467e22f8a70fa9f6972f4874f841a0
josue <jcarmona@emergya.com>
2017-08-29 11:26:49 +0200

    add comments and more

---
+++ mysite/notices/urls.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+#from . models import notice
+
+def index(request)
+    return HttpResponse("Hello, your are in the index notices")
